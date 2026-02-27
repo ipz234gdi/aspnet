9dc9273da6216c2a0061cc4c630790ed30895d53
Reiclid <reiclid@gmail.com>
2026-02-27 10:12:19 +0200

    feat: task4

--- CinemaSln/CinemaBooking/Controllers/HomeController.cs
+++ CinemaSln/CinemaBooking/Controllers/HomeController.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CinemaBooking.Models;
+using CinemaBooking.Models.ViewModels;
+using System.Linq;
 
 namespace CinemaBooking.Controllers
 {
     public class HomeController : Controller
     {
         private ICinemaRepository repository;
+        
+        public int PageSize = 2; 
 
         public HomeController(ICinemaRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult Index() => View(repository.Movies);
+        public ViewResult Index(int moviePage = 1)
+        {
+            var viewModel = new MoviesListViewModel
+            {
+                Movies = repository.Movies
+                    .OrderBy(m => m.MovieID)
+                    .Skip((moviePage - 1) * PageSize)
+                    .Take(PageSize),
+                
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = moviePage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Movies.Count()
+                }
+            };
+
+            return View(viewModel);
+        }
     }
 }
---
+++ CinemaSln/CinemaBooking/Models/ViewModels/MoviesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CinemaBooking.Models.ViewModels
+{
+    public class MoviesListViewModel
+    {
+        public IEnumerable<Movie> Movies { get; set; } = Enumerable.Empty<Movie>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}
