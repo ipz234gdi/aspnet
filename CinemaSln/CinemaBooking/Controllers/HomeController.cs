9d90e03978632fe1542f3ff7c01912ef68af76c6
Reiclid <reiclid@gmail.com>
2026-02-27 10:03:56 +0200

    feat: task3

--- CinemaSln/CinemaBooking/Controllers/HomeController.cs
+++ CinemaSln/CinemaBooking/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CinemaBooking.Models;
 
 namespace CinemaBooking.Controllers
 {
     public class HomeController : Controller
     {
-        // Повертає базове представлення
-        public IActionResult Index() => View();
+        private ICinemaRepository repository;
+
+        public HomeController(ICinemaRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(repository.Movies);
     }
 }
